277ded49c113ca7734e40ab8491d0ce222757df8
CarlosJavierValenzue <carlos.valenzuela.j92@gmail.com>
2024-10-08 10:36:01 +0200

    Commit 3 - diseño interfaz botones e imagenes en botones

--- src/main/java/com/example/ejerciciomenu/HelloController.java
+++ src/main/java/com/example/ejerciciomenu/HelloController.java
@@ -2,7 +2,8 @@ package com.example.ejerciciomenu;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
+
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -10,6 +11,24 @@ import java.util.ResourceBundle;
 public class HelloController implements Initializable {
     @FXML
     private Label welcomeText;
+    @FXML
+    private Button buttonSave;
+    @FXML
+    private Button buttonCancel;
+    @FXML
+    private ChoiceBox genre;
+    @FXML
+    private TextArea description;
+    @FXML
+    private TextField developer;
+    @FXML
+    private TextField title;
+    @FXML
+    private ComboBox platform;
+    @FXML
+    private Spinner year;
+    @FXML
+    private Label info;
 
     @FXML
     protected void onHelloButtonClick() {
